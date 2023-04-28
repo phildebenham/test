b57be00487e3e77d2b248f21c3612c8624f370fd
Phil Debenham <you@yourdomain.example.com>
2023-04-28 11:25:08 +0100

    FIX: multiply_numbers() patched

--- calc.py
+++ calc.py
@@ -8,3 +8,12 @@ Created on Fri Apr 28 10:16:32 2023
 def add_numbers(num_1, num_2):
     result = num_1 + num_2
     return result
+
+def subtract_numbers(num_1, num_2):
+    result = num_1 - num_2
+    return result
+
+def multiply_numbers(num_1, Num_2):
+    result = num_1 * Num_2
+    
+    return result
